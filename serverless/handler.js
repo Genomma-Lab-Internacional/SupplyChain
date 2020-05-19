e5329c4db810a6f7d204ad2f89c75dca83c0c827
caortizpacheco <ca.ortiz.pacheco@gmail.com>
2020-05-19 09:29:44 -0500

    editing lambda

--- serverless/handler.js
+++ serverless/handler.js
@@ -1,9 +1,44 @@
 const AWS = require('aws-sdk'); 
-const connection = require ("./connection")
-AWS.config.update({region: 'us-east-1'});
+const mysql = require ("mysql")
+// const connection = require ("./connection")
+AWS.config.update({region: 'us-east-1'})
 
 module.exports.GetDataMasterInventario = (event,context,callback) => {
   context.callbackWaitsForEmptyEventLoop = false
+
+  const configDB = {
+    host: process.env.host,
+    user: process.env.user,
+    password: process.env.password,
+    port: process.env.port,
+    database: process.env.database,
+    debug: true
+}
+
+function initializeConnection(config) {
+    function addDisconnectHandler(connection) {
+        connection.on("err",err => {
+            if(err instanceof Error) {
+                if(err.code === "PROTOCOL_CONNECTION_LOST") {
+                    console.log(err.stack)
+                    console.log("LOST CONNECTION. RECONNECTING TO GENOMMA LAB DB...")
+                    initializeConnection(connection.config)
+                } else if (err.fatal) {
+                    throw err
+                }
+            }
+        })
+    }
+    const connection = mysql.createConnection(config)
+
+    addDisconnectHandler(connection)
+
+    connection.connect()
+    return connection
+}
+
+const connection = initializeConnection(configDB)
+
   const body = JSON.parse(event.body);
   console.log(body)
   
@@ -31,9 +66,42 @@ module.exports.GetDataMasterInventario = (event,context,callback) => {
 
 module.exports.AddDataProvider = (event,context,callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
+
+  const configDB = {
+    host: process.env.host,
+    user: process.env.user,
+    password: process.env.password,
+    port: process.env.port,
+    database: process.env.database,
+    debug: true
+  }
+
+  function initializeConnection(config) {
+      function addDisconnectHandler(connection) {
+          connection.on("err",err => {
+              if(err instanceof Error) {
+                  if(err.code === "PROTOCOL_CONNECTION_LOST") {
+                      console.log(err.stack)
+                      console.log("LOST CONNECTION. RECONNECTING TO GENOMMA LAB DB...")
+                      initializeConnection(connection.config)
+                  } else if (err.fatal) {
+                      throw err
+                  }
+              }
+          })
+      }
+      const connection = mysql.createConnection(config)
+
+      addDisconnectHandler(connection)
+
+      connection.connect()
+      return connection
+  }
+
+  const connection = initializeConnection(configDB)
   
   const body = JSON.parse(event.body);
-  let provider = body.provider
+  // let provider = body.provider
   let values=[]
   let insert = `INSERT INTO ${body.provider}Inventario (SKUA,descripcion,cantidad,provider) VALUES ?`;
   
@@ -43,102 +111,21 @@ module.exports.AddDataProvider = (event,context,callback) => {
 
   connection.query(insert,[values],(error,rows) => {
     if(error) {
-      console.log("if")
-      // let params1 = {
-      //   Destination: { 
-      //     CcAddresses: ["mario.lopez@genommalab.com","mario.cruz@genommalab.com","daniela.lopez@genommalab.com"],
-      //     ToAddresses: ["carlos.ortiz@genommalab.com"]
-      //   },
-      //   Message: { 
-      //     Body: {
-      //       Html: {
-      //       Charset: "UTF-8",
-      //       Data: `${provider} no pudo subir su inventario. Busca soporte de Inteligencia Artificial`
-      //       },
-      //       Text: {
-      //       Charset: "UTF-8",
-      //       Data: `${provider} no pudo subir su inventario. Busca soporte de Inteligencia Artificial`
-      //       }
-      //     },
-      //     Subject: {
-      //       Charset: 'UTF-8',
-      //       Data: `**${provider}**, ERROR en carga de inventario`
-      //     }
-      //     },
-      //   Source: "mario.lopez@genommalab.com",
-      // }
-
-      // let sendPromise = new AWS.SES({apiVersion: '2010-12-01'}).sendEmail(params1).promise()
-      // sendPromise
-      //   .then( d => {
-      //     console.log("ERRORALCARGARPEROOKALENVIAREMAIL",d)
-          callback({
-            statusCode:500,
-            body:JSON.stringify(error),
-            headers:{
-              "Access-Control-Allow-Origin":"*"
-            }
-          })
-        // })
-        // .catch( e =>{
-        //   console.log("ERRORALCARGARYENVIARMAIL",e)
-        //   callback({
-        //     statusCode:500,
-        //     body:JSON.stringify(error),
-        //     headers:{
-        //       "Access-Control-Allow-Origin":"*"
-        //     }
-        //   })
-        // })
-     
+      callback({
+        statusCode:500,
+        body:JSON.stringify(error),
+        headers:{
+          "Access-Control-Allow-Origin":"*"
+        }
+      })
     } else {
-      console.log("else")
-      // let params = {
-      //   Destination: { 
-      //     CcAddresses: ["mario.lopez@genommalab.com","mario.cruz@genommalab.com","daniela.lopez@genommalab.com"],
-      //     ToAddresses: ["carlos.ortiz@genommalab.com"]
-      //   },
-      //   Message: { 
-      //     Body: {
-      //       Html: {
-      //         Charset: "UTF-8",
-      //         Data: `${body.provider} ya subio su inventario`
-      //       },
-      //       Text: {
-      //         Charset: "UTF-8",
-      //         Data: `${body.provider} ya subio su inventario`
-      //       }
-      //     },
-      //     Subject: {
-      //       Charset: 'UTF-8',
-      //       Data: `**${body.provider}** Ya subio su inventario`
-      //     }
-      //   },
-      //   Source: "mario.lopez@genommalab.com",
-      // }
-      // console.log(params)
-      // let sendPromise = new AWS.SES({apiVersion: '2010-12-01'}).sendEmail(params).promise()
-      // sendPromise
-      //   .then( ( ) => {
-          console.log("EXITOALCARGARYENVIAREMAIL",rows)
-          callback(null,{
-            statusCode:200,
-            body:JSON.stringify("DONE"),
-            headers:{
-              "Access-Control-Allow-Origin":"*"
-            }
-          })
-        // })
-        // .catch( e =>{
-        //   console.log("EXITOALCARGARPERONOALENVIAREMAIL",e)
-        //   callback(null,{
-        //     statusCode:200,
-        //     body:JSON.stringify("DONE"),
-        //     headers:{
-        //       "Access-Control-Allow-Origin":"*"
-        //     }
-        //   }) 
-        // })
+      callback(null, {
+        statusCode:200,
+        body:JSON.stringify("DONE"),
+        headers:{
+          "Access-Control-Allow-Origin":"*"
+        }
+      })
     }
   })
 }
@@ -146,6 +133,39 @@ module.exports.AddDataProvider = (event,context,callback) => {
 module.exports.GetDataProvider = (event,context,callback) => {
   context.callbackWaitsForEmptyEventLoop = false
   
+  const configDB = {
+    host: process.env.host,
+    user: process.env.user,
+    password: process.env.password,
+    port: process.env.port,
+    database: process.env.database,
+    debug: true
+  }
+
+  function initializeConnection(config) {
+      function addDisconnectHandler(connection) {
+          connection.on("err",err => {
+              if(err instanceof Error) {
+                  if(err.code === "PROTOCOL_CONNECTION_LOST") {
+                      console.log(err.stack)
+                      console.log("LOST CONNECTION. RECONNECTING TO GENOMMA LAB DB...")
+                      initializeConnection(connection.config)
+                  } else if (err.fatal) {
+                      throw err
+                  }
+              }
+          })
+      }
+      const connection = mysql.createConnection(config)
+
+      addDisconnectHandler(connection)
+
+      connection.connect()
+      return connection
+  }
+
+  const connection = initializeConnection(configDB)
+
   let provider = JSON.stringify(event.pathParameters)
   let providerEdited = JSON.parse(provider)
   
@@ -175,6 +195,39 @@ module.exports.GetDataProvider = (event,context,callback) => {
 module.exports.AddDataMasterInventario = (event,context,callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
     
+  const configDB = {
+    host: process.env.host,
+    user: process.env.user,
+    password: process.env.password,
+    port: process.env.port,
+    database: process.env.database,
+    debug: true
+}
+
+function initializeConnection(config) {
+    function addDisconnectHandler(connection) {
+        connection.on("err",err => {
+            if(err instanceof Error) {
+                if(err.code === "PROTOCOL_CONNECTION_LOST") {
+                    console.log(err.stack)
+                    console.log("LOST CONNECTION. RECONNECTING TO GENOMMA LAB DB...")
+                    initializeConnection(connection.config)
+                } else if (err.fatal) {
+                    throw err
+                }
+            }
+        })
+    }
+    const connection = mysql.createConnection(config)
+
+    addDisconnectHandler(connection)
+
+    connection.connect()
+    return connection
+}
+
+const connection = initializeConnection(configDB)
+
   const body = JSON.parse(event.body);
   console.log(body)
   
@@ -270,3 +323,57 @@ module.exports.ContactUs = (event, context, callback) => {
     })
 }
 
+
+
+        // })
+        // .catch( e =>{
+        //   console.log("ERRORALCARGARYENVIARMAIL",e)
+        //   callback({
+        //     statusCode:500,
+        //     body:JSON.stringify(error),
+        //     headers:{
+        //       "Access-Control-Allow-Origin":"*"
+        //     }
+        //   })
+        // })
+
+
+              // let params = {
+      //   Destination: { 
+      //     CcAddresses: ["mario.lopez@genommalab.com","mario.cruz@genommalab.com","daniela.lopez@genommalab.com"],
+      //     ToAddresses: ["carlos.ortiz@genommalab.com"]
+      //   },
+      //   Message: { 
+      //     Body: {
+      //       Html: {
+      //         Charset: "UTF-8",
+      //         Data: `${body.provider} ya subio su inventario`
+      //       },
+      //       Text: {
+      //         Charset: "UTF-8",
+      //         Data: `${body.provider} ya subio su inventario`
+      //       }
+      //     },
+      //     Subject: {
+      //       Charset: 'UTF-8',
+      //       Data: `**${body.provider}** Ya subio su inventario`
+      //     }
+      //   },
+      //   Source: "mario.lopez@genommalab.com",
+      // }
+      // console.log(params)
+      // let sendPromise = new AWS.SES({apiVersion: '2010-12-01'}).sendEmail(params).promise()
+      // sendPromise
+      //   .then( ( ) => {
+
+              // })
+        // .catch( e =>{
+        //   console.log("EXITOALCARGARPERONOALENVIAREMAIL",e)
+        //   callback(null,{
+        //     statusCode:200,
+        //     body:JSON.stringify("DONE"),
+        //     headers:{
+        //       "Access-Control-Allow-Origin":"*"
+        //     }
+        //   }) 
+        // })
